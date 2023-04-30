f68fc0979f5d52ffe20953aa88398107ddb14548
butter187 <130690742+butter187@users.noreply.github.com>
2023-04-30 15:57:41 +0300

    Все домашки в Task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -37,3 +37,17 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine("Введите цифру");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 6 || number == 7)
+{
+    Console.WriteLine($"{number} -> да ");
+}
+else if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+{
+    Console.WriteLine($"{number} -> нет ");
+}
+else
+{
+    Console.WriteLine("Введено неверное число ");
+}
